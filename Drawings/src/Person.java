4d30b51e438461fd8efb132ad024c3eb80c67438
Ethan-Bar <70037930+Ethan-Bar@users.noreply.github.com>
2020-09-09 17:18:44 -0700

    Added all the parts of a person and added methods to move and modify the scale of the person

--- Drawings/src/Person.java
+++ Drawings/src/Person.java
@@ -2,5 +2,18 @@
 public class Person {
 
 	// Model after House.
+	public void draw (PApplet surface) {
+		surface.circle(x - 70 * scaleFactor, y + 100 * scaleFactor, 30 * scaleFactor);
+		surface.line(x - 70 * scaleFactor, y + 115 * scaleFactor, x - 70  * scaleFactor, y + 165 * scaleFactor);
+		surface.line(x - 90 * scaleFactor, y + 130 * scaleFactor, x + (20 - 70)  * scaleFactor, y + 130 * scaleFactor);
+		surface.line(x - 70 * scaleFactor, y + 165 * scaleFactor, x + (-15 - 70)  * scaleFactor, y + 200 * scaleFactor);
+		surface.line(x - 70 * scaleFactor, y + 165 * scaleFactor, x + (15 - 70) * scaleFactor, y + 200 * scaleFactor);
+	public void move (int x, int y) {
+		
+		this.x = this.x + x;
+		this.y = this.y + y;
+	public void modifyScale (float amount) {
+		
+		scaleFactor += amount;
 	
 }
