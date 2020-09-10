3387e302ead702c050c18b9cae6fe2cd62b6032c
Ethan-Bar <70037930+Ethan-Bar@users.noreply.github.com>
2020-09-09 17:19:57 -0700

    Draw the person and added if statements that scale and move the house and person according to the key pressed

--- Drawings/src/DrawingSurface.java
+++ Drawings/src/DrawingSurface.java
@@ -20,6 +20,36 @@ public class DrawingSurface extends PApplet {
 	
 	public void draw() {
 		h.draw(this);
+		p.draw(this);
+	}
+	
+	public void keyPressed () {
+		
+		background(205,205,205);
+		if (keyCode == KeyEvent.VK_W) {
+			h.modifyScale(0.1f);
+			p.modifyScale(0.1f);
+		} else if (keyCode == KeyEvent.VK_S) {
+			h.modifyScale(-0.1f);
+			p.modifyScale(-0.1f);
+		}
+		
+		if (keyCode == KeyEvent.VK_RIGHT) {
+			h.move(10, 0);
+			p.move(10, 0);
+		} else if (keyCode == KeyEvent.VK_LEFT) {
+			h.move(-10, 0);
+			p.move(-10, 0);
+		}
+		
+		if (keyCode == KeyEvent.VK_UP) {
+			h.move(0, -10);
+			p.move(0, -10);
+		} else if (keyCode == KeyEvent.VK_DOWN) {
+			h.move(0, 10);
+			p.move(0, 10);
+		}
+		
 	}
 	
 	// Add methods for user interaction. Check the processing reference for info on this (http://processing.org).
