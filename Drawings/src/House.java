cc4d372e72b0519f27279cca8986170f80fbc4f1
Ethan-Bar <70037930+Ethan-Bar@users.noreply.github.com>
2020-09-09 17:17:27 -0700

    Added all the parts of a house and methods to move and modify the house

--- Drawings/src/House.java
+++ Drawings/src/House.java
@@ -14,6 +14,11 @@ public class House {
 	public void draw(PApplet surface) {
 		
 		surface.rect(x, y, 300, 200);
+		surface.rect(x, y, 300 * scaleFactor, 200 * scaleFactor);
+		surface.triangle(x, y, x + 150 * scaleFactor, y - 100 * scaleFactor, x + 300 * scaleFactor, y);
+		surface.square(x + 50 *scaleFactor, y + 50 * scaleFactor, 50 * scaleFactor);
+		surface.square(x + 200 *scaleFactor, y + 50 * scaleFactor, 50 * scaleFactor);
+		surface.rect(x + 115 * scaleFactor, y + 100 * scaleFactor, 70 * scaleFactor, 100 * scaleFactor);
 		
 	}
 
@@ -21,6 +26,14 @@ public class House {
 	public void move(int x, int y) {
 		this.x = x;
 		this.y = y;
+		
+		this.x = this.x + x;
+		this.y = this.y + y;
+	}
+	
+	public void modifyScale (float amount) {
+		
+		scaleFactor += amount;
 	}
 	// Add more methods to modify fields. Make sure methods sound like real actions.
 	
