367fef7c231f2a91febf8f214c2a91b8ad450c0e
Ethan-Bar <70037930+Ethan-Bar@users.noreply.github.com>
2020-09-18 20:33:52 -0700

    Changed some of the buttons that control the scaling and movement of the hosue and person

--- Drawings/src/DrawingSurface.java
+++ Drawings/src/DrawingSurface.java
@@ -28,34 +28,45 @@ public class DrawingSurface extends PApplet {
 		pushStyle();
 		
 		textSize(10);
-		text("Interactions: Arrow Keys control the movement of the house and person\n W and S control the scaling of the house and person", 10, 10);
+		text("Interactions: Arrow Keys control the movement of the person     "
+				+ "W, A, S, and D control the movement of the house\n"
+				+ "Space increases the scaling of the person and house     "
+				+ "BackSpace decreases the scaling of the person and house", 10, 10);
 	}
 	
 	public void keyPressed () {
 		
 		background(205,205,205);
-		if (keyCode == KeyEvent.VK_W) { //W and S move everything up or down
+		if (keyCode == KeyEvent.VK_SPACE) { //W and S move everything up or down
 			h.modifyScale(0.1f);
 			p.modifyScale(0.1f);
-		} else if (keyCode == KeyEvent.VK_S) {
+		} else if (keyCode == KeyEvent.VK_BACK_SPACE) {
 			h.modifyScale(-0.1f);
 			p.modifyScale(-0.1f);
 		}
 		
 		if (keyCode == KeyEvent.VK_RIGHT) { //Right and left arrows move everything left or right
-			h.move(10, 0);
 			p.move(10, 0);
 		} else if (keyCode == KeyEvent.VK_LEFT) {
-			h.move(-10, 0);
 			p.move(-10, 0);
 		}
 		
+		if (keyCode == KeyEvent.VK_D) { //D and A move the house right or left
+			h.move(10, 0);
+		} else if (keyCode == KeyEvent.VK_A) {
+			h.move(-10, 0);
+		}
+		
 		if (keyCode == KeyEvent.VK_UP) { //Up and down arrows move everything left or right
-			h.move(0, -10);
 			p.move(0, -10);
 		} else if (keyCode == KeyEvent.VK_DOWN) {
-			h.move(0, 10);
 			p.move(0, 10);
+		}
+		
+		if (keyCode == KeyEvent.VK_W) { //Up and down arrows move everything left or right
+			h.move(0, -10);
+		} else if (keyCode == KeyEvent.VK_S) {
+			h.move(0, 10);
 		}
 		
 	}
